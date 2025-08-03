1ca05590d4e45e3dd391ded7ff8228618a7a0967
skiyee <319619193@qq.com>
2025-08-03 14:48:13 +0800

    feat(utils): 新增一个关于性能的基础工具

---
+++ packages/skiyee-uni-ui/src/utils/performance.ts
@@ -0,0 +1,104 @@
+/**
+ * 防抖函数
+ */
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null = null
+
+  return (...args: Parameters<T>) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => func(...args), wait)
+  }
+}
+
+/**
+ * 节流函数
+ */
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): (...args: Parameters<T>) => void {
+  let lastTime = 0
+
+  return (...args: Parameters<T>) => {
+    const now = Date.now()
+    if (now - lastTime >= wait) {
+      lastTime = now
+      func(...args)
+    }
+  }
+}
+
+/**
+ * 深度克隆
+ */
+export function deepClone<T>(obj: T): T {
+  if (obj === null || typeof obj !== 'object') {
+    return obj
+  }
+
+  if (obj instanceof Date) {
+    return new Date(obj.getTime()) as T
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => deepClone(item)) as T
+  }
+
+  if (typeof obj === 'object') {
+    const cloned = {} as T
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        cloned[key] = deepClone(obj[key])
+      }
+    }
+    return cloned
+  }
+
+  return obj
+}
+
+/**
+ * 全类型比较
+ */
+export function isEqual(a: any, b: any): boolean {
+  if (a === b) {
+    return true
+  }
+
+  if (a == null || b == null) {
+    return a === b
+  }
+
+  if (typeof a !== typeof b) {
+    return false
+  }
+
+  if (typeof a !== 'object') {
+    return a === b
+  }
+
+  if (Array.isArray(a) !== Array.isArray(b)) {
+    return false
+  }
+
+  if (Array.isArray(a)) {
+    if (a.length !== b.length) {
+      return false
+    }
+    return a.every((item, index) => isEqual(item, b[index]))
+  }
+
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
+
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+
+  return keysA.every(key => isEqual(a[key], b[key]))
+}
