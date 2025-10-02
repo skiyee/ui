1e4d9bf22a1912b00e3b58d422a4265ad6080b57
skiyee <319619193@qq.com>
2025-10-02 16:50:05 +0800

    docs: 优化字体的加载

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -4,6 +4,19 @@ export default defineNuxtConfig({
   extends: [
     ['github:skiyee/skdocs', { auth: process.env.GITHUB_TOKEN }],
   ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preload',
+          as: 'font',
+          type: 'font/woff2',
+          href: '/fonts/PuHuiTiSans.woff2',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       demo: 'bundle',
